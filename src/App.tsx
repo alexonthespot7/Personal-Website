37e99d6997fda62eb6f96ee3229f3c47896e5843
Aleksei Shevelenkov <aleksei.shevelenkov@sievo.com>
2026-09-12 01:15:48 +0300

    Add experience section

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { useEffect, useRef } from 'react';
 import MyNav from './Components/MyNav';
 import useMediaQuery from './Hooks/useMediaQuery';
 import About from './Components/About';
+import WorkExperience from './Components/WorkExperience';
 import Portfolio from './Components/Portfolio';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
@@ -16,19 +17,21 @@ export default function App() {
   const ref = useRef<HTMLDivElement>(null);
   const refHome = useRef<HTMLElement>(null);
   const refAbout = useRef<HTMLElement>(null);
+  const refExp = useRef<HTMLElement>(null);
   const refPort = useRef<HTMLElement>(null);
   const refCont = useRef<HTMLElement>(null);
   const inView = useInView(ref);
   const inViewHome = useInView(refHome, { amount: 0 });
   const inViewAbout = useInView(refAbout, { amount: 0 });
+  const inViewExp = useInView(refExp, { amount: 0 });
   const inViewPort = useInView(refPort, { amount: 0 });
   const inViewCont = useInView(refCont, { amount: 0 });
 
   const defineActive = () => {
     if (matchesS) {
-      return (inView || inViewHome) ? 'Home' : inViewAbout ? 'About' : inViewPort ? 'Portfolio' : inViewCont ? 'Contact' : '';
+      return (inView || inViewHome) ? 'Home' : inViewAbout ? 'About' : inViewExp ? 'Experience' : inViewPort ? 'Portfolio' : inViewCont ? 'Contact' : '';
     } else {
-      return inViewHome ? 'Home' : inViewAbout ? 'About' : inViewPort ? 'Portfolio' : inViewCont ? 'Contact' : '';
+      return inViewHome ? 'Home' : inViewAbout ? 'About' : inViewExp ? 'Experience' : inViewPort ? 'Portfolio' : inViewCont ? 'Contact' : '';
     }
   }
 
@@ -46,7 +49,10 @@ export default function App() {
       <section style={{ backgroundColor: '#ffffff' }} ref={refAbout} id='About'>
         <About />
       </section>
-      <section style={{ backgroundColor: '#f5f5f5' }} ref={refPort} id='Projects'>
+      <section style={{ backgroundColor: '#f5f5f5' }} ref={refExp} id='Experience'>
+        <WorkExperience />
+      </section>
+      <section style={{ backgroundColor: '#ffffff' }} ref={refPort} id='Projects'>
         <Portfolio />
       </section>
       <section style={{ backgroundColor: '#252934' }} ref={refCont} id="Contact">
--- src/Components/MyNav.tsx
+++ src/Components/MyNav.tsx
@@ -38,7 +38,8 @@ export default function MyNav({ active, position, inView }: MyNavProps) {
                         <Toolbar sx={{ display: 'flex', alignItems: 'center', gap: 10, justifyContent: 'center' }} >
                             <HashLink smooth to="#" className={active === 'Home' ? 'active' : 'link'}>HOME</HashLink>
                             <HashLink smooth to="#About" className={active === 'About' ? 'active' : 'link'}>ABOUT</HashLink>
-                            <HashLink smooth to="#Projects" className={active === 'Port' ? 'active' : 'link'}>PORTFOLIO</HashLink>
+                            <HashLink smooth to="#Experience" className={active === 'Experience' ? 'active' : 'link'}>EXPERIENCE</HashLink>
+                            <HashLink smooth to="#Projects" className={active === 'Portfolio' ? 'active' : 'link'}>PORTFOLIO</HashLink>
                             <HashLink smooth to="#Contact" className={active === 'Contact' ? 'active' : 'link'}>CONTACT</HashLink>
                         </Toolbar>
                     </AppBar>
@@ -52,6 +53,7 @@ export default function MyNav({ active, position, inView }: MyNavProps) {
                             <Toolbar sx={{ display: 'flex', alignItems: 'center', gap: 10, justifyContent: 'center' }} >
                                 <HashLink smooth to="#Home" className={active === 'Home' ? 'active' : 'link'}>HOME</HashLink>
                                 <HashLink smooth to="#About" className={active === 'About' ? 'active' : 'link'}>ABOUT</HashLink>
+                                <HashLink smooth to="#Experience" className={active === 'Experience' ? 'active' : 'link'}>EXPERIENCE</HashLink>
                                 <HashLink smooth to="#Projects" className={active === 'Portfolio' ? 'active' : 'link'}>PORTFOLIO</HashLink>
                                 <HashLink smooth to="#Contact" className={active === 'Contact' ? 'active' : 'link'}>CONTACT</HashLink>
                             </Toolbar>
@@ -74,6 +76,7 @@ export default function MyNav({ active, position, inView }: MyNavProps) {
                     <div className={`Nav ${open && "Active"}`}>
                         <HashLink onClick={handleClose} smooth to="#Home" className={active === 'Home' ? 'active Mini Home' : 'link Mini Home'}>HOME</HashLink>
                         <HashLink onClick={handleClose} smooth to="#About" className={active === 'About' ? 'active Mini' : 'link Mini'}>ABOUT</HashLink>
+                        <HashLink onClick={handleClose} smooth to="#Experience" className={active === 'Experience' ? 'active Mini' : 'link Mini'}>EXPERIENCE</HashLink>
                         <HashLink onClick={handleClose} smooth to="#Projects" className={active === 'Portfolio' ? 'active Mini' : 'link Mini'}>PORTFOLIO</HashLink>
                         <HashLink onClick={handleClose} smooth to="#Contact" className={active === 'Contact' ? 'active Mini' : 'link Mini'}>CONTACT</HashLink>
                     </div>
