b1fea29025a368b8e02accba158c6dd54f2d41cb
Aleksei Shevelenkov <aleksei.shevelenkov@sievo.com>
2026-09-12 01:17:00 +0300

    Remove inst and telegram links

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,8 +4,6 @@ import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp
 import { HashLink } from 'react-router-hash-link';
 import { RiLinkedinFill } from 'react-icons/ri';
 import { RiGithubFill } from 'react-icons/ri';
-import { SiTelegram } from 'react-icons/si';
-import { RiInstagramLine } from 'react-icons/ri';
 import useMediaQuery from '../Hooks/useMediaQuery';
 
 export default function Footer() {
@@ -28,12 +26,6 @@ export default function Footer() {
                 </div>
                 <div onClick={() => openLink('https://github.com/alexonthespot7')} className='SocialLink'>
                     <RiGithubFill size={24} />
-                </div>
-                <div onClick={() => openLink('https://t.me/axosinc')} className='SocialLink'>
-                    <SiTelegram size={24} />
-                </div>
-                <div onClick={() => openLink('https://www.instagram.com/mark_eto_leha_dobav')} className='SocialLink'>
-                    <RiInstagramLine size={24} />
                 </div>
             </div>
             <div className='FooterInfo'>
